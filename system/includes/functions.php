d976116dd44d8fe02fe01e6715ad1ea839b8586e
Mohamad Fahmi <mohamadf4hmi@gmail.com>
2014-01-02 15:42:30 +0700

    Update Google Universal Analytics Function

--- system/includes/functions.php
+++ system/includes/functions.php
@@ -600,10 +600,16 @@ function publisher(){
 	}
 }
 
+// Google Universal Analytics
 function analytics(){
 	$analytics = config('google.analytics.js');
+	$script = <<<EOF
+	<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', '{$analytics}');
+ga('send', 'pageview');</script>
+EOF;
 	if (!empty($analytics)) {
-		return $analytics;
+		return $script;
 	}
 }
 
