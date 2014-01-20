2ccb5be0ace88f69c4ab290b19b919dd213e6455
Danang Probo Sayekti <dan@danpros.com>
2014-01-20 13:00:52 +0700

    Change config dir
    Change config DIR.

--- system/htmly.php
+++ system/htmly.php
@@ -10,7 +10,7 @@ require 'system/includes/functions.php';
 require 'system/includes/opml.php';
 
 // Load the configuration file
-config('source', 'system/config.ini');
+config('source', 'admin/config.ini');
 
 // The front page of the blog.
 // This will match the root url
