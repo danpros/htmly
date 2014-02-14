c7fb74859f7e000ba851fce54b627d06fd803bbc
Danang Probo Sayekti <dan@danpros.com>
2014-02-15 01:19:25 +0700

    Clean up
    Clean up

--- system/resources/htmly.js
+++ system/resources/htmly.js
@@ -1,7 +1,7 @@
 (function ($) {
 
 	$(document).ready(function() {
-		$('.post-teaser img, .post-body img').each(function() {
+		$('.teaser-body img, .post-body img').each(function() {
 			var currentImage = $(this);
 			currentImage.wrap("<a class='img-wrap' title='" + currentImage.attr("alt") + "' data-lightbox='lightbox' href='" + currentImage.attr("src") + "'></a>");
 		});
