5eefbe93f8555a82dc0cf3cec38e5eac9bcc924d
Kanti <git@kanti.de>
2014-12-14 18:39:52 +0100

    [TASK] added Feature #120

--- system/includes/functions.php
+++ system/includes/functions.php
@@ -1758,3 +1758,24 @@ function remove_html_comments($content)
 {
     return trim(preg_replace('/(\s|)<!--(.*)-->(\s|)/', '', $content));
 }
+
+function isCaptcha($reCaptchaResponse){
+    if(! config("google.reCaptcha")){
+        return true;
+    }
+    $url = "https://www.google.com/recaptcha/api/siteverify";
+    $options = array(
+        "secret" => config("google.reCaptcha.private"),
+        "response" => $reCaptchaResponse,
+        "remoteip" => $_SERVER['REMOTE_ADDR'],
+    );
+    $fileContent = @file_get_contents($url . "?" . http_build_query($options));
+    if($fileContent === false) {
+        return false;
+    }
+    $json = json_decode($fileContent, true);
+    if($json == false){
+        return false;
+    }
+    return ($json['success']);
+}
