3ded899febfe0b6eb66d51c77883ce3a36f5cb39
Kanti <git@kanti.de>
2014-12-17 08:43:03 +0100

    [TASK] add upgrade specific stuff (e.g.: delete vendor/)

--- system/admin/views/updated-to.html.php
+++ system/admin/views/updated-to.html.php
@@ -1,3 +1,7 @@
 <h3>Updated to<h3>
 <h2>[<?php echo $info['tag_name'];?>] <?php echo $info['name']; ?></h2>
 <p><?php echo $info['body'];?></p>
+
+<?php
+require_once "system/upgrade/run.php";
+?>
---
+++ system/upgrade/run.php
@@ -0,0 +1,40 @@
+<?php
+
+$updater = new Kanti\HubUpdater("danpros/htmly");
+$info = $updater->getCurrentInfo();
+$versionNumber = substr($info['tag_name'],1);
+
+function isGraterThan($string){
+    global $versionNumber;
+    return (version_compare($versionNumber,$string) > 0);
+}
+
+// http://stackoverflow.com/questions/3338123/how-do-i-recursively-delete-a-directory-and-its-entire-contents-files-sub-dir
+function rrmdir($dir) {
+    if (is_dir($dir)) {
+        $objects = scandir($dir);
+        foreach ($objects as $object) {
+            if ($object != "." && $object != "..") {
+                if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object);
+            }
+        }
+        reset($objects);
+        rmdir($dir);
+    }
+}
+
+//run upgrade specific stuff
+if(isGraterThan("2.3")) {// 2.4, 2.5, ...
+    echo "yes";
+    if(file_exists("vendor/")){
+        rrmdir("vendor/");
+    }
+}
+else{
+    echo "no";
+}
+
+if(isGraterThan("2.3")) {
+    file_put_contents("index.php",file_get_contents("system/upgrade/index.php"));
+    rrmdir("system/upgrade/");
+}
