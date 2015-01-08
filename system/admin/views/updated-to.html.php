ae3cfef99ad97f0e3456b21d0f4bf5e720e4e4bc
Kanti <git@kanti.de>
2015-01-08 13:54:45 +0100

    [TASK] markdown parser added to updatet-to page

--- system/admin/views/updated-to.html.php
+++ system/admin/views/updated-to.html.php
@@ -1,6 +1,6 @@
 <h3>Updated to<h3>
 <h2>[<?php echo $info['tag_name'];?>] <?php echo $info['name']; ?></h2>
-<p><?php echo $info['body'];?></p>
+<p><?php echo \Michelf\MarkdownExtra::defaultTransform($info['body']);?></p>
 
 <?php
 require_once "system/upgrade/run.php";
