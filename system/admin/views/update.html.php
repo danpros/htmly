0729260d37455d5fa590fd3dc858f5ef1ddf4901
Danang Probo Sayekti <dan@danpros.com>
2015-07-26 11:49:18 +0700

    Add more info to update page

--- system/admin/views/update.html.php
+++ system/admin/views/update.html.php
@@ -11,9 +11,14 @@ $updater = new HubUpdater(array(
     
 if ($updater->able()) {
     $info = $updater->getNewestInfo();
-    echo '<h3>Update Available</h3>';
-    echo '<p><a href="' . site_url() . 'admin/update/now/' . $CSRF . '" alt="' . $info['name'] . '">Update to ' . $info['tag_name'] . '</a></p>';
+    echo '<h2>Update Available</h2>';
+    echo '<h3>'. $info['name'] .'</h3>';
+    echo '<p>Version: '. $info['tag_name'] .'</p>';
+    echo '<h4>Release Notes</h4>';
+    echo '<pre><code>'. $info['body'] .'</code></pre>';
+    echo '<p><strong>Important:</strong> Please always backup your files before upgrading to newer version.</p>';
+    echo '<p><a href="' . site_url() . 'admin/update/now/' . $CSRF . '" alt="' . $info['name'] . '">Update to ' . $info['tag_name'] . ' now</a></p>';
 } else {
     echo '<h3>No Available Update</h3>';
-    echo '<p>You are using the latest HTMLy version.</p>';    
+    echo '<p>You are using the latest HTMLy version.</p>';  
 }
