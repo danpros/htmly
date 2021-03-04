ce5e7d0b7b39f97ee962234849916cf35e01642f
danpros <dan@danpros.com>
2021-03-04 22:37:26 +0700

    Update no-posts.html.php

--- themes/twentyfifteen/no-posts.html.php
+++ themes/twentyfifteen/no-posts.html.php
@@ -1,5 +1,5 @@
 <article class="post type-post hentry ">
-	<header class="entry-header"
+	<header class="entry-header">
 	<h1 class="entry-title"><?php echo i18n('No_posts_found');?>!</h1>
 	</header>
     <div class="entry-content">
