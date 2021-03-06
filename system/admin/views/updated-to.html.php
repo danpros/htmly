c08e4d51ca1cc4fbb6c1b11717eeac79acace4b9
danpros <dan@danpros.com>
2021-03-06 23:42:51 +0700

    Update updated-to.html.php

--- system/admin/views/updated-to.html.php
+++ system/admin/views/updated-to.html.php
@@ -1,4 +1,3 @@
-<?php if (!defined('HTMLY')) die('HTMLy'); ?>
 <h3>Updated to<h3>
 <h2>[<?php echo $info['tag_name']; ?>] <?php echo $info['name']; ?></h2>
 <p><?php echo \Michelf\MarkdownExtra::defaultTransform($info['body']); ?></p>
