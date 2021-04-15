0bc0931eff75773b15ae95133f31b523aae47313
danpros <dan@danpros.com>
2021-04-15 12:05:45 +0700

    Improve set cookie params
    Add conditional statement. Before PHP 7.3 it only receive string

--- install.php
+++ install.php
@@ -222,7 +222,14 @@ if(from($_SERVER,'QUERY_STRING') == "rewriteRule.html")
     echo "YES!";
     die();
 }
-session_set_cookie_params(['samesite' => 'Strict']);
+
+$samesite = 'strict';
+if(PHP_VERSION_ID < 70300) {
+    session_set_cookie_params('samesite='.$samesite);	
+} else {
+    session_set_cookie_params(['samesite' => $samesite]);
+}
+
 session_start();
 new Settings;
 
--- system/includes/session.php
+++ system/includes/session.php
@@ -1,4 +1,11 @@
 <?php
+$samesite = 'strict';
+if(PHP_VERSION_ID < 70300) {
+    session_set_cookie_params('samesite='.$samesite);	
+} else {
+    session_set_cookie_params(['samesite' => $samesite]);
+}
+
 session_set_cookie_params(['samesite' => 'Strict']);
 if (isset($_COOKIE['PHPSESSID']))
     session_start();
