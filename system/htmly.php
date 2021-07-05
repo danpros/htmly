87a24145dcfe0a5da83b7699a48b9a63d5a573f6
Yaya Laressa <yayalaressa2015@gmail.com>
2021-07-05 22:02:06 +0700

    Update htmly.php

--- system/htmly.php
+++ system/htmly.php
@@ -1484,11 +1484,6 @@ post('/admin/config/performance', function () {
         save_config($new_config, $new_Keys);
         
         // Clear cache after setting performance
-        $files = array();
-        $draft = array();
-        $files = glob('content/*/blog/*.md', GLOB_NOSORT);
-        $draft = glob('content/*/draft/*.md', GLOB_NOSORT);
-        rebuilt_cache('all');
         foreach (glob('cache/page/*.cache', GLOB_NOSORT) as $file) {
         	unlink($file);
         }
