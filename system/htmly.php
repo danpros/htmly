5bf1fdd2d7e5def2cc4c58ecd263f04090630b2e
Yaya Laressa <yayalaressa2015@gmail.com>
2021-07-05 15:12:13 +0700

    Fix /admin/config/performance

--- system/htmly.php
+++ system/htmly.php
@@ -1482,6 +1482,17 @@ post('/admin/config/performance', function () {
             }
         }
         save_config($new_config, $new_Keys);
+        
+        // Clear cache after setting performance
+        $files = array();
+        $draft = array();
+        $files = glob('content/*/blog/*.md', GLOB_NOSORT);
+        $draft = glob('content/*/draft/*.md', GLOB_NOSORT);
+        rebuilt_cache('all');
+        foreach (glob('cache/page/*.cache', GLOB_NOSORT) as $file) {
+        	unlink($file);
+        }
+        
         $login = site_url() . 'admin/config/performance';
         header("location: $login");
     } else {
