17a140cf0fa818f8a8f3f60ed3bbf4edcd200970
vallyol <vallyol@users.noreply.github.com>
2021-11-05 16:52:45 +0300

    Update login.html.php
    replacing text with key values from language files

--- system/admin/views/login.html.php
+++ system/admin/views/login.html.php
@@ -11,14 +11,14 @@
             if (empty($username)) {
                 echo 'error';
             }
-        } ?>" name="user" placeholder="Username"/>
+        } ?>" name="user" placeholder="<?php echo i18n('User'); ?>"/>
 		<br>
         <label><?php echo i18n('Password');?> <span class="required">*</span></label>
         <input type="password" class="form-control <?php if (isset($password)) {
             if (empty($password)) {
                 echo 'error';
             }
-        } ?>" name="password" placeholder="Password"/>
+        } ?>" name="password" placeholder="<?php echo i18n('Password'); ?>"/>
 		<br>
         <input type="hidden" name="csrf_token" value="<?php echo get_csrf() ?>">
         <?php if (config('google.reCaptcha') === 'true'): ?>
