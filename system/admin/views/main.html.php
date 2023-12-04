3eebfe2044eed0a69fe2b1496f31d2a6764b99eb
danpros <dan@danpros.com>
2023-12-04 23:43:53 +0700

    Move function to individual views

--- system/admin/views/main.html.php
+++ system/admin/views/main.html.php
@@ -3,4 +3,39 @@
 <br>
 <a class="btn btn-primary right" href="<?php echo site_url();?>admin/content"><?php echo i18n('Add_content');?></a>
 <br><br>
-<?php get_user_posts();?>
+<?php 
+
+if (isset($_SESSION[config("site.url")]['user'])) {
+	$posts = get_profile_posts($_SESSION[config("site.url")]['user'], 1, 5);
+	if (!empty($posts)) {
+		echo '<table class="table post-list">';
+		echo '<tr class="head"><th>' . i18n('Title') . '</th><th>' . i18n('Published') . '</th>';
+		if (config("views.counter") == "true")
+			echo '<th>'.i18n('Views').'</th>';
+		echo '<th>' . i18n('Category') . '</th><th>' . i18n('Tags') . '</th><th>' . i18n('Operations') . '</th></tr>';
+		$i = 0;
+		$len = count($posts);
+		foreach ($posts as $p) {
+			if ($i == 0) {
+				$class = 'item first';
+			} elseif ($i == $len - 1) {
+				$class = 'item last';
+			} else {
+				$class = 'item';
+			}
+			$i++;
+			echo '<tr class="' . $class . '">';
+			echo '<td><a target="_blank" href="' . $p->url . '">' . $p->title . '</a></td>';
+			echo '<td>' . format_date($p->date) . '</td>';
+			if (config("views.counter") == "true")
+				echo '<td>' . $p->views . '</td>';
+			echo '<td><a href="' . str_replace('category', 'admin/categories', $p->categoryUrl) . '">'. strip_tags($p->category) .'</a></td>';
+			echo '<td>' . $p->tag . '</td>';
+			echo '<td><a class="btn btn-primary btn-xs" href="' . $p->url . '/edit?destination=admin">' . i18n('Edit') . '</a> <a class="btn btn-danger btn-xs" href="' . $p->url . '/delete?destination=admin">' . i18n('Delete') . '</a></td>';
+			echo '</tr>';
+		}
+		echo '</table>';
+	}
+}
+
+?>
--- system/admin/views/static-pages.html.php
+++ system/admin/views/static-pages.html.php
@@ -3,4 +3,50 @@
 <br>
 <a class="btn btn-primary right" href="<?php echo site_url();?>add/page"><?php echo i18n('Add_new_page');?></a>
 <br><br>
-<?php get_user_pages(); ?>
+<?php 
+
+if (isset($_SESSION[config("site.url")]['user'])) {
+	$posts = get_static_post(null);
+	if (!empty($posts)) {
+		krsort($posts);
+		echo '<table class="table post-list">';
+		echo '<tr class="head"><th>' . i18n('Title') . '</th>';
+		if (config("views.counter") == "true")
+			echo '<th>'.i18n('Views').'</th>';
+		echo '<th>' . i18n('Operations') . '</th></tr>';
+		$i = 0;
+		$len = count($posts);
+		foreach ($posts as $p) {
+			if ($i == 0) {
+				$class = 'item first';
+			} elseif ($i == $len - 1) {
+				$class = 'item last';
+			} else {
+				$class = 'item';
+			}
+			$i++;
+
+			echo '<tr class="' . $class . '">';
+			echo '<td><a target="_blank" href="' . $p->url . '">' . $p->title . '</a></td>';
+			if (config("views.counter") == "true")
+				echo '<td>' . $p->views . '</td>';
+			echo '<td><a class="btn btn-primary btn-xs" href="' . $p->url . '/add?destination=admin/pages">' . i18n('Add_sub') . '</a> <a class="btn btn-primary btn-xs" href="' . $p->url . '/edit?destination=admin/pages">' . i18n('Edit') . '</a> <a class="btn btn-danger btn-xs" href="' . $p->url . '/delete?destination=admin/pages">' . i18n('Delete') . '</a></td>';
+			echo '</tr>';
+
+			$shortUrl = substr($p->url, strrpos($p->url, "/") + 1);
+			$subPages = get_static_sub_post($shortUrl, null);
+
+			foreach ($subPages as $sp) {
+				echo '<tr class="' . $class . '">';
+				echo '<td> <span style="margin-left:30px;">&raquo; <a target="_blank" href="' . $sp->url . '">' . $sp->title . '</a></span></td>';
+				if (config("views.counter") == "true")
+					echo '<td>' . $sp->views . '</td>';
+				echo '<td><a class="btn btn-primary btn-xs" href="' . $sp->url . '/edit?destination=admin/pages">' . i18n('Edit') . '</a> <a class="btn btn-danger btn-xs" href="' . $sp->url . '/delete?destination=admin/pages">' . i18n('Delete') . '</a></td>';
+				echo '</tr>';
+			}
+		}
+		echo '</table>';
+	}
+}
+
+ ?>
