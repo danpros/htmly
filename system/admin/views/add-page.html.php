72e53f25b1993fe5308e8e1295a3fbc8c185ef0d
danpros <dan@danpros.com>
2023-12-06 15:20:25 +0700

    Update add-page.html.php

--- system/admin/views/add-page.html.php
+++ system/admin/views/add-page.html.php
@@ -20,12 +20,9 @@
 					<label for="pTitle"><?php echo i18n('Title');?> <span class="required">*</span></label>
 					<input type="text" class="form-control text <?php if (isset($postTitle)) {if (empty($postTitle)) {echo 'error';}} ?>" id="pTitle" name="title" value="<?php if (isset($postTitle)) {echo $postTitle;} ?>"/>
 					<br>
-					<?php if ($type == 'is_page') :?>
 					<label for="pMeta"><?php echo i18n('Meta_description');?> (<?php echo i18n('optional');?>)</label>
 					<textarea id="pMeta" class="form-control" name="description" rows="3" cols="20" placeholder="<?php echo i18n('If_leave_empty_we_will_excerpt_it_from_the_content_below');?>"><?php if (isset($p->description)) {echo $p->description;} ?></textarea>
 					<br>
-					<?php endif;?>
-					
 				</div>
 				<div class="col-sm-6">
 					<?php if ($type == 'is_page') :?>
