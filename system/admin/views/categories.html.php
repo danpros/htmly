92f272d20e174f1a23c175b274fe6998894e6959
Robert Riebisch <15858666+bttrx@users.noreply.github.com>
2023-12-09 19:41:23 +0100

    Sort categories alphabetically in 'Categories' view

--- system/admin/views/categories.html.php
+++ system/admin/views/categories.html.php
@@ -1,5 +1,8 @@
 <?php if (!defined('HTMLY')) die('HTMLy'); ?>
-<?php $desc = get_category_info(null); ?>
+<?php
+$desc = get_category_info(null);
+asort($desc);
+?>
 <h2><?php echo i18n("Categories");?></h2>
 <br>
 <a class="btn btn-primary " href="<?php echo site_url();?>add/category"><?php echo i18n('Add_category');?></a>
