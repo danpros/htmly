e5e87717f4c530c0c9b59be5c54601a2caf80687
danpros <dan@danpros.com>
2024-01-26 14:19:22 +0700

    Change version

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 define('HTMLY', true);
-define('HTMLY_VERSION', 'v2.9.0');
+define('HTMLY_VERSION', 'v2.9.1');
 $config_file = 'config/config.ini';
 require 'system/vendor/autoload.php';
 require 'system/htmly.php';
