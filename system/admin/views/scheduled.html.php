5eeaa1b7518ccb5e20643c842a439325dce24f7d
Huang95 <xd9527@gmail.com>
2024-02-26 14:17:10 +0800

    Update scheduled.html.php , Let tag list look like tag/label
    Let tag list look like tag/label

--- system/admin/views/scheduled.html.php
+++ system/admin/views/scheduled.html.php
@@ -20,7 +20,7 @@
                 <td><?php echo $p->title ?></td>
                 <td><?php echo format_date($p->date, 'd F Y, H:i:s') ?></td>
                 <td><a href="<?php echo str_replace('category', 'admin/categories', $p->categoryUrl); ?>"><?php echo strip_tags($p->category);?></a></td>
-                <td><?php echo $p->tag ?></td>
+                <td><?php echo str_replace('rel="tag"', 'rel="tag" class="badge badge-light text-primary font-weight-normal"', $p->tag); ?></td>
                 <td><a class="btn btn-primary btn-xs" href="<?php echo $p->url ?>/edit?destination=admin/scheduled"><?php echo i18n('Edit');?></a> <a class="btn btn-danger btn-xs" href="<?php echo $p->url ?>/delete?destination=admin/scheduled"><?php echo i18n('Delete');?></a></td>
             </tr>
         <?php endforeach; ?>
