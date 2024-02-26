4fd6d5ec29b34902cc058bc33a186ba25fc5a3f5
Huang95 <xd9527@gmail.com>
2024-02-26 14:16:17 +0800

    Update user-posts.html.php , Let tag list look like tag/label
    Let tag list look like tag/label

--- system/admin/views/user-posts.html.php
+++ system/admin/views/user-posts.html.php
@@ -26,7 +26,7 @@
                     <td><?php echo $p->views ?></td>
                 <?php endif; ?>
                 <td><a href="<?php echo str_replace('category', 'admin/categories', $p->categoryUrl); ?>"><?php echo strip_tags($p->category);?></a></td>
-                <td><?php echo $p->tag ?></td>
+                <td><?php echo str_replace('rel="tag"', 'rel="tag" class="badge badge-light text-primary font-weight-normal"', $p->tag); ?></td>
                 <td><a class="btn btn-primary btn-xs" href="<?php echo $p->url ?>/edit?destination=admin/mine"><?php echo i18n('Edit');?></a> <a class="btn btn-danger btn-xs" href="<?php echo $p->url ?>/delete?destination=admin/mine"><?php echo i18n('Delete');?></a></td>
             </tr>
         <?php endforeach; ?>
