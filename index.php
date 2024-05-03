19b25f33cc8f450b435cb35214c59c8748434397
danpros <dan@danpros.com>
2024-05-03 07:47:06 +0700

    Update version

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 define('HTMLY', true);
-define('HTMLY_VERSION', 'v2.9.7');
+define('HTMLY_VERSION', 'v2.9.8');
 $config_file = 'config/config.ini';
 require 'system/vendor/autoload.php';
 require 'system/htmly.php';
