10bc30112f66be58518b80a7fa9e40a73c3eb431
danpros <dan@danpros.com>
2024-11-11 13:04:17 +0700

    Sidebar always dark + add missing lang string

--- system/admin/views/layout.html.php
+++ system/admin/views/layout.html.php
@@ -54,7 +54,7 @@ if (isset($_GET['search'])) {
   <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
-  <aside class="main-sidebar <?php echo ((config('admin.theme') === 'light' || is_null(config('admin.theme'))) ? "sidebar-light-primary" : "sidebar-dark-primary"); ?> elevation-4">
+  <aside class="main-sidebar sidebar-dark-primary> elevation-4">
 
 
     <!-- Sidebar -->
@@ -348,6 +348,7 @@ if (isset($_GET['search'])) {
     </div>
     <!-- /.login-card-body -->
   </div>
+  <br>
   <span><a href="<?php echo site_url();?>"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
 </svg> <?php echo i18n('Back_to'); ?> <?php echo blog_title();?></a></span>
