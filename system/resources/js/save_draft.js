a0b8059647656616f694d11b0413ef3e261c5774
andrigamerita <37557992+andrigamerita@users.noreply.github.com>
2025-06-07 15:35:01 +0200

    Refactor autosave draft script and add error handling

--- system/resources/js/save_draft.js
+++ system/resources/js/save_draft.js
@@ -1,43 +1,29 @@
-const response = document.getElementById("response");
+function hideError() {
+  $("#response-error").fadeOut(600, function() {
+    $("#response-error").css("display", "none");
+  });
+}
 
 function updateData() {
-  var title = $("#pTitle").val();
-  var url = $("#pURL").val();
-  var content = $("#wmd-input").val();
-  var description = $("#pMeta").val();
-  var tag = $("#pTag").val();
-  var category = $("#pCategory").val();
-  var posttype = $("#pType").val();
-  var pimage = $("#pImage").val();
-  var paudio = $("#pAudio").val();
-  var pvideo = $("#pVideo").val();
-  var pquote = $("#pQuote").val();
-  var plink = $("#pLink").val();
-  var pDate = $("#pDate").val();
-  var pTime = $("#pTime").val();
-  var oldfile = $("#oldfile").val();
-  var dateTime = pDate + " " + pTime;
-  var autoSave = 'autoSave';
-
   // Prepare data to send to PHP
   var data = {
-    title: title,
-    url: url,
-    content: content,
-    description: description,
-    tag: tag,
-    category: category,
-    posttype: posttype,
-    pimage: pimage,
-    paudio: paudio,
-    pvideo: pvideo,
-    pquote: pquote,
-    plink: plink,
-    dateTime: dateTime,
-    autoSave: autoSave,
+    title: $("#pTitle").val(),
+    url: $("#pURL").val(),
+    content: $("#wmd-input").val(),
+    description: $("#pMeta").val(),
+    tag: $("#pTag").val(),
+    category: $("#pCategory").val(),
+    posttype: $("#pType").val(),
+    pimage: $("#pImage").val(),
+    paudio: $("#pAudio").val(),
+    pvideo: $("#pVideo").val(),
+    pquote: $("#pQuote").val(),
+    plink: $("#pLink").val(),
+    dateTime: $("#pDate").val() + " " + $("#pTime").val(),
+    autoSave: 'autoSave',
     addEdit: addEdit,
-    oldfile: oldfile,
-	parent_page: parent_page
+    oldfile: $("#oldfile").val(),
+    parent_page: parent_page
   };
   
   $.ajax({
@@ -47,6 +33,7 @@ function updateData() {
     success: function(response) {
       $("#response").html(response.message);
       $("#oldfile").val(response.file);
+      hideError();
       $("#response").fadeIn(600, function() {
         $("#response").css("display", "block");
       });
@@ -55,6 +42,13 @@ function updateData() {
           $("#response").css("display", "none");
         });
       }, 6000);
+    },
+    error: function(response) {
+      $("#response-error").html("Error in Autosaving: " + response.statusText);
+      $("#response-error").fadeIn(600, function() {
+        $("#response-error").css("display", "block");
+      });
+      setTimeout(hideError, saveInterval/2);
     }
   });
 }
