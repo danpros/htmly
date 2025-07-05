bac48639e90533075da09455f8f91a1f3f6c410d
Dan <danpros@live.com>
2025-07-05 08:09:08 +0700

    No posts message

--- themes/twentyfifteen/no-posts.html.php
+++ themes/twentyfifteen/no-posts.html.php
@@ -4,5 +4,6 @@
 	<h1 class="entry-title"><?php echo i18n('No_posts_found');?>!</h1>
 	</header>
     <div class="entry-content">
+		<p>Create a blog post or set the frontpage display to static page to remove this message.</p>
     </div>
 </article>
